239737c973a7dd840c2204adb5b57a91609710bb
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2020-02-16 22:49:48 -0300

    Initial

---
+++ index.js
@@ -0,0 +1,71 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const block = (data) => {
+    process.on('uncaughtException', function (err) {
+        console.log('Caught exception: ' + err)
+    })
+    rl.on('SIGINT', () => {
+        console.log(data)
+        rl.pause()
+    })
+}
+
+console.clear();
+
+const folder = [
+    {"folder": "Root"},
+    {"folder": "users"},
+    {"folder": "documents"}
+]
+
+var user = {
+            "user": "Root",
+            "password": "toor",
+            "admin": 0}
+var file = {"folder": "Root"}
+
+const commands = [
+    {"command": "cd", "function": 
+    function explore(onde){
+        console.log(onde)
+    }},
+    {"command": "clear", 
+    "function": function clear(){
+        console.log("teste")
+        console.clear();
+    }}
+]
+
+var commandSend = "";
+
+function command(){ 
+    rl.question("\x1b[40m"+"\x1b[32m"+"@"+user.user+"\x1b[0m"+"/"+"\x1b[2m"+file.folder+"$ ", (answer) => {
+        // TODO: Log the answer in a database
+        commandSend = answer;
+        if(commands.find(command => command.command === commandSend)){
+            const found = commands.find(command => command.command === commandSend);
+            const id = commands.indexOf(found)
+            commands[id].function()
+        } else{
+            console.log(commandSend+": "+"\x1b[2m"+"não é um comando");
+        }
+        if(commandSend !== ""){
+            commandSend = "";
+        } else{
+            commandSend = "";
+            return false;
+        }
+        console.log("\x1b[0m");
+        command();
+    }); 
+}
+
+function start(){
+    command();
+}
+
+start();
